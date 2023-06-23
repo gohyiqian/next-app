1b4726d2554ec629bccb7712c3f835631f55a56d
gohyiqian <kenji.gohyiqian@gmail.com>
2023-06-23 18:06:32 +0800

    update

--- src/pages/portfolios/index.tsx
+++ src/pages/portfolios/index.tsx
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,7 +10,12 @@ export default function Portfolio() {
     >
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Portfolio Page
+          Portfolio Page&nbsp;
+          <Link href="/">
+            <code className="font-mono font-bold">
+              {process.env.NEXT_PUBLIC_BASE_PATH}/Home
+            </code>
+          </Link>
         </p>
       </div>
     </main>
