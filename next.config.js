00fe850aa00a9d5aabc96a7a58621468c6f3ab6a
gohyiqian <kenji.gohyiqian@gmail.com>
2023-06-24 18:25:14 +0800

    test basepath

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   env: {
     NEXT_PUBLIC_BASE_PATH: process.env.NEXT_PUBLIC_BASE_PATH,
@@ -9,6 +10,18 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  // output: "standalone",
+  // redirect does not work for static export
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: "/",
+  //       destination: process.env.NEXT_PUBLIC_BASE_PATH,
+  //       basePath: false,
+  //       permanent: false,
+  //     },
+  //   ];
+  // },
 };
 
 module.exports = nextConfig;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import Link from "next/link";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  const { basePath } = router;
+  console.log("basePath:", basePath);
+  console.log("pageProps:", pageProps);
+  return <Component {...pageProps} />;
 }
